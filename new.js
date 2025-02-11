a6216ca758dc8b4bfe03346d0dbada8b22abff2b
itshr12 <preethr12@gmail.com>
2025-02-12 00:26:36 +0530

    Update new.js

--- new.js
+++ new.js
@@ -3,12 +3,19 @@ function tab(){
     var qq = document.getElementById('rlt');
     qq.innerHTML = ' ';
     q=parseFloat(document.getElementById('h').value);
-    for(w=1;w<=10;w++)
-    {
+    if(q>0 && q<999999){
+        for(w=1;w<=10;w++){
         var el=q*w;
         var qq=document.getElementById('rlt');
         qq.innerHTML+=`${q} X ${w} = ${el} <br>`;
     }
+    }
+    else{
+        var qq=document.getElementById('rlt');
+        qq.innerHTML+=`0-999999`;
+    }
+    
+
     document.getElementById('h').value = '';
 }
 
